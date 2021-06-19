aeeceb63051aa26ab390ede1c194899482dbe009
rezanasu <rezanasution82@gmail.com>
2021-06-19 07:15:58 +0700

    implement login

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::API
+    
+  
+    def encode_token(payload)
+        JWT.encode(payload, 'SECRET')
+    end
+
+    def auth_header
+        request.headers['Authorization']
+    end
+
+    def decoded_token
+        if auth_header
+          token = auth_header.split(' ')[1]
+          # header: { 'Authorization': 'Bearer <token>' }
+          begin
+            JWT.decode(token, 'SECRET', true, algorithm: 'HS256')
+          rescue JWT::DecodeError
+            nil
+          end
+        end
+    end
+
 end
--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -1,9 +1,22 @@
 class UsersController < ApplicationController
+    # REGISTER
     def create
         user = User.create(
             email: params[:email],
             password: params[:password]
         )
         render json: user
+    end
+
+    # LOGIN
+    def login
+        @user = User.find_by(email: params[:email])
+
+        if @user && @user.authenticate(params[:password])
+            token = encode_token({user_id: @user.id})
+            render json: {user: @user, token: token}
+        else 
+            render json: {error: "Invalid username or password"}
+        end
     end
 end
