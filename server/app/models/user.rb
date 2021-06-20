eef18849e7aed7fa29d6615d20c680e08ec3a975
rezanasu <rezanasution82@gmail.com>
2021-06-20 21:44:45 +0700

    Delete server directory

--- server/app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-    has_many :tasks, dependent: :destroy
-    has_secure_password
-    validates :email, presence: true, uniqueness: true
-    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP}
-end
